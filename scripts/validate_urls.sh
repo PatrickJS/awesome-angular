e9a689e49e0a59d1418519345f5b0f9ad5f45ad3
rolandjitsu <rolandjitsu@gmail.com>
2016-11-06 11:23:30 +0200

    chore(validate_urls.sh): remove whitespace after comma

--- scripts/validate_urls.sh
+++ scripts/validate_urls.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-awesome_bot README.md --white-list angularclass.com, \
-  angular.io, \
-  github.com/angularclass/awesome-angular2, \
-  github.com/angular/universal, \
-  github.com/angular/material2, \
-  blog.thoughtram.io, \
-  github.com/angular/react-native-renderer, \
-  github.com/FuelInteractive/fuel-ui, \
+awesome_bot README.md --white-list angularclass.com,\
+  angular.io,\
+  github.com/angularclass/awesome-angular2,\
+  github.com/angular/universal,\
+  github.com/angular/material2,\
+  blog.thoughtram.io,\
+  github.com/angular/react-native-renderer,\
+  github.com/FuelInteractive/fuel-ui,\
   babeljs.io

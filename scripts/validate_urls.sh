9f60a181fd291e35a460b7f5dd243ff4061b9eda
rolandjitsu <rolandjitsu@gmail.com>
2016-11-06 11:37:47 +0200

    chore(validate_urls.sh): add more urls to whitelist

--- scripts/validate_urls.sh
+++ scripts/validate_urls.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-awesome_bot README.md --white-list angularclass.com,angular.io,github.com/angularclass/awesome-angular2,github.com/angular/universal,github.com/angular/material2,blog.thoughtram.io,github.com/angular/react-native-renderer,github.com/FuelInteractive/fuel-ui,babeljs.io
+WHITELIST_URLS=angularclass.com,\
+angular.io,\
+https://dartlang-slack.herokuapp.com/,\
+github.com/angularclass/awesome-angular2,\
+github.com/angular/universal,\
+github.com/angular/material2,\
+https://github.com/michaelbazos/angular2-starter,\
+blog.thoughtram.io,\
+github.com/angular/react-native-renderer,\
+github.com/FuelInteractive/fuel-ui,\
+babeljs.io,\
+ng-book.com
+
+awesome_bot README.md --white-list ${WHITELIST_URLS}
